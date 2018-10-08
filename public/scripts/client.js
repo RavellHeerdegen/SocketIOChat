3a1b7a9d190c00341d1988e1becc4dbddf036f1c
Ravell Heerdegen <43538667+RavellHeerdegen@users.noreply.github.com>
2018-10-08 19:50:07 +0200

    Initial commit

---
+++ public/scripts/client.js
@@ -0,0 +1,176 @@
+
+
+// loginVariables START
+var loginButton = $("#loginButton");
+var usernameInput = $("#usernameInput");
+// loginVariables END
+
+// logoutVariables START
+var logoutButton = $("#logoutButton");
+// logoutVariables END
+
+// sendVariables START
+var message = $("#message");
+var messageSendButton = $("#messageSendButton");
+// sendVariables END
+
+// chatVariables START
+var activeroom;
+var activechatwindow;
+// chatVariables END
+
+// DIVs START
+var loginDiv = $("#loginDiv");
+var chatDiv = $("#chatDiv").hide();
+var chatMessagesWindow = $("#chatMessagesWindow");
+// DIVs END
+
+// Personal data START
+var username;
+var userId;
+var rooms = [];
+// Personal data END
+
+/* Click Events START */
+
+/**
+ * Handles the login of a user and registers the user on the server
+ */
+loginButton.click(() => {
+    username = usernameInput.val();
+    userId = socket.id;
+    socket.emit("login", {username: username, userid: userId});
+});
+
+/**
+ * Handles the logout of a user
+ */
+logoutButton.click((callback) => {
+    socket.emit("disconnect");
+    callback = loadLogoutConfiguration;
+    callback();
+});
+
+/**
+ * Emits an allchat_message to the server
+ */
+messageSendButton.click((callback) => {
+    socket.emit("send", {message: message.val(), room: activeroom});
+    callback = () => {message.val("");}
+    callback();
+});
+
+/* CLick Events END */
+
+/* Socket.on Events START */
+
+/**
+ * Handles the send_all socket-event of the server
+ */
+socket.on("send", (data) => {
+    document.getElementById(data.room + "window").innerHTML += data.message;
+});
+
+/**
+ * Handles the disconnect_all socket-event of the server
+ */
+socket.on("disconnect", (data) => {
+    document.getElementById("usersonlinelist").innerHTML = data.usersonlinelist;
+    document.getElementById("loggedInUsersCount").innerHTML = data.loggedinusers;
+    document.getElementById("AllChatwindow").innerHTML += data.userdisconnectedstring;
+});
+
+/**
+ * Handles the login_successful socket-event of the server
+ */
+socket.on("login_successful", (data, callback) => {
+    if (data.username === username) {
+        document.getElementById("loggedInUserName").innerHTML = data.loggedinas;
+        callback = loadLoginConfiguration;
+        callback(data);
+    } else {
+        document.getElementById("usersonlinelist").innerHTML = data.usersonlinelist;
+        document.getElementById("loggedInUsersCount").innerHTML = data.loggedinusers;
+        document.getElementById("AllChatwindow").innerHTML += data.userconnectedstring;
+    }
+});
+
+/**
+ * Handles the login_failed event and gives feedback to the requesting client
+ */
+socket.on("login_failed", (data) => {
+    document.getElementById("loginFailedLabel").innerHTML = data.text;
+});
+
+/**
+ * Handles the creation event for a new private room, creates a new room and corresponding window
+ */
+socket.on("established_private_room", (data, callback) => {
+    console.log("Establish event erhalten CLient");
+    rooms.push(data.room);
+    if (data.requestorsocketname === username) {
+        activeroom = data.room;
+        activechatwindow = data.room + "window";
+        document.getElementById("chatMessagesWindow").innerHTML += data.newchatwindow;
+        for (i = 0; i < rooms.length; i++) {
+            if (rooms[i] !== activeroom){
+                document.getElementById(rooms[i] + "window").style.display = "none";
+            }
+        }
+        document.getElementById(activechatwindow).style.display = "block";
+    } else {
+        document.getElementById("chatMessagesWindow").innerHTML += data.newchatwindow;
+        document.getElementById(data.room + "window").style.display = "none";
+    }
+    callback = () => { socket.emit("update_chattabs", {
+        username: username
+    })};
+    callback();
+});
+
+socket.on("update_chattabs", (data) => {
+    console.log("Update chattabs bei client erhalten");
+    document.getElementById("roomsTabsWindow").innerHTML = data.chattabs;
+});
+
+/* Socket.on Events END */
+
+/* Functions START */
+
+/**
+ * Loads up the login configuration of the DOM-Elements
+ */
+function loadLoginConfiguration(data, callback) {
+    activeroom = "AllChat";
+    activechatwindow = "AllChatwindow";
+    rooms.push("AllChat");
+    document.getElementById("chatMessagesWindow").innerHTML += data.newchatwindow;
+    document.getElementById("usersonlinelist").innerHTML = data.usersonlinelist;
+    document.getElementById("loggedInUsersCount").innerHTML = data.loggedinusers;
+    document.getElementById("AllChatwindow").innerHTML += data.userconnectedstring;
+    loginDiv.hide();
+    chatDiv.show();
+    socket.emit("update_chattabs", {
+        username: username
+    });
+}
+
+/**
+ * Loads up the logout configuration of the DOM-Elements
+ */
+function loadLogoutConfiguration() {
+    loginDiv.show();
+    chatDiv.hide();
+}
+
+/* Functions END */
+
+function switchChatTabs(newactivechatname) {
+    console.log("Switching tabs");
+    rooms.forEach(room => {
+        document.getElementById(room + "window").style.display = "none";
+    });
+    activeroom = newactivechatname.id;
+    activechatwindow = newactivechatname.id + "window";
+    document.getElementById(newactivechatname.id + "window").style.display = "block";
+}
---
+++ server.js
@@ -0,0 +1,199 @@
+/* Initialisation of all modules and prototypes START */
+const express = require("express"); //Get module express
+const app = express(); //Instantiate a prototype of express
+
+app.use(express.static(__dirname + "/public")); //Default path for route-tracing is public/...
+
+// Server variables START
+var users = [];
+// Server variables END
+
+/* Start Server */
+server = app.listen(3000, function() {
+    console.log('Server running on port 3000');
+  });
+
+const io = require("socket.io")(server); // Socket is attached to server
+
+/* Routes START */
+
+/**
+ * Called when someone requests the homepage
+ */
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/index.html");
+});
+
+/* Routes END */
+
+/* Initialisation of all modules and prototypes END */
+
+/* IO Handlings (user connects to server) START */
+
+/**
+ * Called when a new socket connects to the server
+ */
+io.on("connection", (socket) => {
+    //Socket is the connection to the user
+    socket.username = "Anonymous";
+    
+    /**
+     * Event triggered when receiving a login-emit of a client
+     */
+    socket.on("login", (data, callback) => {
+        callback = emitLoginEvent;
+        callback(socket, data);
+    });
+
+    /**
+     * Creates a private room for two chat partners and emits it to the client for visual feedback
+     */
+    socket.on("create_private_room", (data) => {
+        ownUsername = socket.username;
+        otherUsername = data.othername;
+        roomname = ownUsername + otherUsername;
+        socket.join(roomname);
+        othersocket = users.find(f => f.username === otherUsername);
+        othersocket.join(roomname);
+        buildChatTabs(socket.username);
+        buildChatTabs(othersocket.username);
+        io.in(roomname).emit("established_private_room", {
+            room: roomname,
+            requestorsocketname: ownUsername,
+            newchatwindow: "<ul class='list-group' id='" + roomname + "window" + "'></ul>"
+        });
+    });
+
+    socket.on("update_chattabs", (data, callback) => {
+        callback = buildChatTabs;
+        callback(data.username);
+    });
+
+    /**
+     * Event triggered when receiving a send_all-emit of a client
+     */
+    socket.on("send", (data) => {
+        io.in(data.room).emit("send", {
+            message: "<li class='list-group-item'>" + socket.username + ": " + 
+                data.message + "<small class='text-info'>" + " " +  new Date().toUTCString() + "</small>" + "</li>",
+            room: data.room 
+        });
+    });
+
+    /**
+     * Event triggered when closing the tab, logging out or timing out or refreshing page
+     */
+    socket.on("disconnect", (callback) => {
+        callback = emitLogoutEvent;
+        callback(socket);
+    });
+
+});
+
+/* IO Handlings END */
+
+/* Callbacks and Event Handlings START */
+
+/**
+ * Proofes if the given username is already taken by another loggedin user
+ * @param {string} the username of the connecting client
+ */
+function proofUsername(username) {
+    usernameFound = false;
+    for(i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Handles the login process of checking for valid username and redirecting if login is successful or not
+ * @param {Socket} socket the connecting client socket
+ * @param {any} data the login data like username and socket.id
+ */
+function emitLoginEvent(socket, data) {
+    usernameFound = proofUsername(data.username);
+    if (usernameFound) {
+        socket.emit('login_failed', {text: "Username already taken."}); //send fail-emit to sender socket
+    } else {
+        socket.username = data.username;
+        socket.id = data.userid;
+        socket.join("AllChat");
+        users.push(socket);
+        activeuserscontent = buildOnlineUsersList();
+        io.in("AllChat").emit("login_successful", { // emit to all users in allchat-room
+            username: socket.username,
+            loggedinas: "Logged in as " + socket.username, 
+            loggedinusers: "Number of users: " + users.length,
+            userconnectedstring: "<li class='list-group-item text-success'>" +
+                socket.username + " has connected"  + "</li>",
+            usersonlinelist: activeuserscontent,
+            newchatwindow: "<ul class='list-group' id='AllChatwindow'></ul>"
+        });
+    } 
+}
+
+/**
+ * Handles the logout process like disconnecting the client and inform the other users
+ * @param {Socket} socket the disconnecting client socket
+ */
+function emitLogoutEvent(socket) {
+    var index;
+    var disconnectingUser;
+    for (i = 0; i < users.length; i++) {
+        if (users[i].username === socket.username) {
+            index = i;
+            disconnectingUser = users[i].username;
+        }
+    }
+    users.splice(index, 1);
+    activeuserscontent = buildOnlineUsersList();
+    io.in("AllChat").emit("disconnect", { // emit to all users in allchat-room
+        userdisconnectedstring: "<li class='list-group-item text-danger'>" + disconnectingUser +
+            " has disconnected" + "</li>", 
+        loggedinusers: "Number of users: " + users.length,
+        usersonlinelist: activeuserscontent
+    });
+}
+
+/**
+ * Builds the client-sided list of active users to build private chats with
+ */
+function buildOnlineUsersList() {
+    content = "";
+    for (i = 0; i < users.length; i++) {
+        content = content + 
+            "<div class='row'><button type='button' class='btn btn-default' id='" + 
+            users[i].username + "'" + " onclick='createPrivateRoom(" + users[i].username  + ")'>" +
+            users[i].username + 
+            "</button></div>";
+    }
+    return content;
+}
+
+/**
+ * 
+ * @param {string} username the username of the socket (client)
+ */
+function buildChatTabs(username) {
+    socket = users.find(f => f.username === username);
+    console.log("Found user with name:" + socket.username);
+    chatTabsDOMElements = "";
+    rooms = Object.keys(socket.rooms);
+    rooms.forEach(room => {
+        if (room != socket.id) {
+            chatTabDOM = "<button type='button' class='btn btn-default' id='" + room + 
+                "' onclick='switchChatTabs(" + room  + ")'>" + room + "</button>";
+            chatTabsDOMElements += chatTabDOM;
+        }
+    });
+    console.log("Emitting to" + socket.id);
+    socket.emit("update_chattabs", {
+        chattabs: chatTabsDOMElements
+    });
+}
+
+/* Callbacks and Event Handlings END */
+
