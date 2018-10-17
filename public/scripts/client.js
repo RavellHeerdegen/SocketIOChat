8fb444866b3b6b056b3beb077ddaf2ec8267fa7a
Jan <Jan@DESKTOP-FII5AQE>
2018-10-17 20:45:18 +0200

    Frontend funktionen und anordnung

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -279,3 +279,16 @@ function Room() {
 }
 
 /* Object models END */
+
+/* NavBar START */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+/* NavBar END */
