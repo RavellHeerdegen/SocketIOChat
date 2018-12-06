ffe80a4dc0ae3f8c6efe034cf09939e1cc3c0a44
Ravell Heerdegen <ravell.heerdegen@student.reutlingen-university.de>
2018-12-06 09:40:08 +0100

    Update ..encrypting passwords

--- modules/database_module.js
+++ modules/database_module.js
@@ -1,25 +1,26 @@
 /* JAN POHL 761383, RAVELL HEERDEGEN 761330 */
 
 const mysql = require('mysql');
+var bcrypt = require('bcryptjs');
 
 //MySQL Database connection
 let connection = mysql.createConnection('mysql://admin:EKAVNZNWEVTYOGSX@sl-eu-fra-2-portal.5.dblayer.com:18372/compose');
 
 // TABLE QUERIES
 // let getallusersquery = "select * from users";
-// let deleterowsquery = "delete from users;";
+let deleterowsquery = "delete from users;";
 // let addcolumnquery = "alter table users ADD profilepictureID VARCHAR(100);";
-// return new Promise((resolve, reject) => {
-//     connection.query(getallusersquery, (err, rows) => {
-//         if (err || !rows[0]) {
-//             resolve(false);
-//         } else {
-//             if (rows[0].username) {
-//                 resolve(true);
-//             }
-//         }
-//     });
-// });
+return new Promise((resolve, reject) => {
+    connection.query(deleterowsquery, (err, rows) => {
+        if (err || !rows[0]) {
+            resolve(false);
+        } else {
+            if (rows[0].username) {
+                resolve(true);
+            }
+        }
+    });
+});
 
 // QUERIES END
 
@@ -58,14 +59,18 @@ function login(username, password) {
             if (err || !rows[0]) {
             } else {
                 if (rows[0].username && rows[0].password) {
-                    if (password === rows[0].password) {
-                        if (rows[0].profilepic && rows[0].profilepic !== "") {
-                            result.result = true;
-                            result.profilepic = rows[0].profilepic;
+                    bcrypt.compare(password, hash, function(err, res) {
+                        if (res) {
+                            if (rows[0].profilepic && rows[0].profilepic !== "") {
+                                result.result = true;
+                                result.profilepic = rows[0].profilepic;
+                            } else {
+                                result.result = true;
+                            }
                         } else {
-                            result.result = true;
+
                         }
-                    }
+                    });
                 }
             }
             resolve(result);
@@ -81,13 +86,17 @@ function login(username, password) {
 function register(username, password) {
     return new Promise((resolve, reject) => {
         if (username && password) {
-            let query = 'insert into user (username,password) values ("' + username + '","' + password + '");';
-            return connection.query(query, (err) => {
-                if (err) {
-                    resolve(false);
-                } else {
-                    resolve(true);
-                }
+            bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.hash(password, salt, function (err, hash) {
+                    let query = 'insert into user (username,password) values ("' + username + '","' + hash + '");';
+                    return connection.query(query, (err) => {
+                        if (err) {
+                            resolve(false);
+                        } else {
+                            resolve(true);
+                        }
+                    });
+                });
             });
         } else resolve(false);
     });
@@ -102,13 +111,17 @@ function register(username, password) {
 function registerWithPic(username, password, pictureblob) {
     return new Promise((resolve, reject) => {
         if (username && password && pictureblob) {
-            let query = 'insert into user (username,password,profilepic) values ("' + username + '","' + password + '","' + pictureblob + '" );';
-            return connection.query(query, (err) => {
-                if (err) {
-                    resolve(false);
-                } else {
-                    resolve(true);
-                }
+            bcrypt.genSalt(10, function (err, salt) {
+                bcrypt.hash(password, salt, function (err, hash) {
+                    let query = 'insert into user (username,password,profilepic) values ("' + username + '","' + hash + '","' + pictureblob + '" );';
+                    return connection.query(query, (err) => {
+                        if (err) {
+                            resolve(false);
+                        } else {
+                            resolve(true);
+                        }
+                    });
+                });
             });
         } else resolve(false);
     });
